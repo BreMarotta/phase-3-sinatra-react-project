059f6ce4bb735e1f6ff1ec4326f18692d2ff04ab
Breanne <BreMarotta@gmail.com>
2022-03-17 21:00:48 -0400

    add owner creations to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
-
+Owner.create(name: "Carmen", color: "#2F4F4F")
+Owner.create(name: "Breanne", color: "#8B008B")
+Owner.create(name: "Caius", color: "#00008B")
+Owner.create(name: "Nikeo", color: "#FF4500")
+Owner.create(name: "Franco", color: "#6B8E23")
+Owner.create(name: "Ralphie", color: "#40E0D0")
+Owner.create(name: "WishList", color: "#FFFFFF")
 puts "✅ Done seeding!"
