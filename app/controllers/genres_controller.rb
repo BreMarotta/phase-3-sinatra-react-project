83c599b64be55dfe31bfb81663ca119454c70614
Breanne <BreMarotta@gmail.com>
2022-03-19 22:13:06 -0400

    add additional genres and build out database

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,7 +2,7 @@ class GenresController < ApplicationController
 
     get '/genres' do 
         genres = Genre.all 
-        genres.to_json
+        genres.to_json(include: :lego_sets)
     end
 
     # On dropdown menu for new_lego_set form will populate genres via GET to '/genres'. Will have an "other" option that will need to allow for a new genre to be added.
--- app/controllers/lego_sets_controller.rb
+++ app/controllers/lego_sets_controller.rb
@@ -1,37 +1,32 @@
 class LegoSetsController < ApplicationController 
 
-    # Sets are sorted in ascending order by the set_number. Cannot get this to include both owner and genre. The code below works for each individually but not together. It will only put the second one if they are both listed.
+    # Sets are sorted in ascending order by the set_number. Cannot get this to include both owner and genre.
     get '/lego_sets' do
         lego_sets = LegoSet.sort_order
-        lego_sets.to_json
-        # ({
-        #     include: { 
-        #         owner: { only: [:name]}},  
-        #     include: {
-        #         genre: { only: [:name]}}
-        # })
+        lego_sets.to_json(include: :owner)  
     end
 
-    # Will need to create a controlled form to add new lego_set. On form, owner and genre need to be dropdown populated by GET requests to /owners and /genres. For Genres, will need an "other" option that will send a POST to /genres to create a new genre?
+    # Will need to create a controlled form to add new lego_set. On form, owner and genre need to be dropdown populated by GET requests to /owners and /genres (will be in MyContext). For Genres, will need an "other" option that will send a POST to /genres to create a new genre?
     post '/lego_sets' do
-        new_set = LegoSet.create(
+        owner = Owner.find_by(id: params[:owner_id])
+        new_set = owner.lego_sets.create(
             set_number: params[:set_number],
             pieces: params[:pieces],
             name: params[:name],
             img: params[:img],
-            owner_id: params[:owner_id],
             genre_id: params[:genre_id]
         )
-        new_set.to_json
+        new_set.to_json(include: :owner)
     end
 
     # Patch will be used to update owner of a set. Primarily when a set on the WishList is gifted to a new owner. Button on set card will need a dropdown to select new owner. That will send a PATCH to lego_sets/:id (id being the lego_set_id) with the new owner_id.
     patch '/lego_sets/:id' do 
+        # owner = Owner.find_by(id: params[:id])
         lego_set = LegoSet.find_by(id: params[:id])
         lego_set.update(
             owner_id: params[:owner_id]
         )
-        lego_set.to_json
+        lego_set.to_json(include: :owner)
     end
     
 end
